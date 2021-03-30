5639dadb15d963ee532b4595e616a5fa0cdf6507
Adrian Gajda <adrian.gajda@hotmail.com>
2021-03-31 00:18:47 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
+
+const items = [
+  {
+    title: "What is React",
+    content: "React is front end js framework",
+  },
+  {
+    title: "Czy JP2 GMD?",
+    content: "Jeszcze jak",
+  },
+  {
+    title: "Papiezowa godzina?",
+    content: "21;37",
+  },
+];
+
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+
+  {
+    label: "A shade of Blue",
+    value: "blue",
+  },
+];
+
+export default () => {
+  return (
+    <div>
+      <Dropdown options={options} />
+    </div>
+  );
+};
