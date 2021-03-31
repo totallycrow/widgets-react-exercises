d03e5d0723258140bc7ddb736c6136ba98ccd6f5
Adrian Gajda <adrian.gajda@hotmail.com>
2021-04-01 00:27:41 +0100

    Add selected piece of state and setter for the dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
@@ -36,9 +36,14 @@ const options = [
 ];
 
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
-      <Dropdown options={options} />
+      <Dropdown
+        options={options}
+        selected={selected}
+        onSelectedChange={setSelected}
+      />
     </div>
   );
 };
